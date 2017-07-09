8f56903d8d736449246315ce3fdcaf2a114cf073
Bart Wijnants <bartwijnants@outlook.com>
2017-07-09 09:22:43 +0200

    production build

---
+++ webpack.production.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
